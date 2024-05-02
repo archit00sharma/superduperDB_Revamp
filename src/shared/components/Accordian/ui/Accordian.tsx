6a5188a8e694df9f4de723fc8d976f143cb5adf8
archit00sharma <architsharma29@gmail.com>
2024-05-02 22:01:07 +0530

    accordian modified

--- src/shared/components/Accordian/ui/Accordian.tsx
+++ src/shared/components/Accordian/ui/Accordian.tsx
@@ -1,26 +1,31 @@
 import React, { useState } from 'react';
 
-
 interface AccordionProps {
-  items: any;
+  items: any[];
 }
 
-
 const Accordion: React.FC<AccordionProps> = ({ items }) => {
-  const [activeIndex, setActiveIndex] = useState(null);
-  const [displayCount, setDisplayCount] = useState(3);
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const [displayCount, setDisplayCount] = useState<number>(3);
+  const [showLess, setShowLess] = useState<boolean>(false);
 
-  const onAccordionClick = (index: any) => {
+  const onAccordionClick = (index: number) => {
     setActiveIndex(activeIndex === index ? null : index);
   };
 
   const showMore = () => {
     setDisplayCount(items.length);
+    setShowLess(true);
+  };
+
+  const showLessHandler = () => {
+    setDisplayCount(3);
+    setShowLess(false);
   };
 
   return (
     <div className="info-list-wrapper">
-      {items.slice(0, displayCount).map((item: any, index: any) => (
+      {items.slice(0, displayCount).map((item, index) => (
         <div key={index}>
           <div className="frame20">
             <div className="info-details" onClick={() => onAccordionClick(index)}>
@@ -54,9 +59,14 @@ const Accordion: React.FC<AccordionProps> = ({ items }) => {
           <div className="info-items" />
         </div>
       ))}
-      {displayCount < items.length && (
+      {displayCount < items.length && !showLess && (
         <div onClick={showMore} className="show-more">
           Show more
+        </div>
+      )}
+      {showLess && (
+        <div onClick={showLessHandler} className="show-more">
+          Show less
         </div>
       )}
     </div>
